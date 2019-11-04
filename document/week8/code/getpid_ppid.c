1fb99f995cae12088a36e7275783084470d5c767
WTBABC <935110142@qq.com>
2019-11-04 04:07:56 -0800

    delete fileOperation week8

--- document/week8/code/getpid_ppid.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int main(int argc, char* argv[], char* env[]){
-	printf("ppid = %d, pid = %d\n", getppid(), getpid());
-	while (1);
-	return 0;
-}
